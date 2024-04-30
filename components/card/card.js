fe5963f115abda031565c61458aa4d63e7d04207
Torben Jost <torbenjost@proton.me>
2024-04-30 10:07:54 +0200

    activate js modules, let funciton createCharacterCard create a l ist, attach it to a button event listener to be able to console.log in browser and test

--- components/card/card.js
+++ components/card/card.js
@@ -1,1 +1,8 @@
-export function createCharacterCard() {}
+const cardContainer = document.querySelector('[data-js="card-container"]');
+
+export function createCharacterCard() {
+  const card = document.createElement("list");
+  card.setAttribute("class", "card");
+  let card_testInput = "please work";
+  cardContainer.append(card);
+}
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { createCharacterCard } from "./components/card/card.js";
+
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
@@ -7,6 +9,10 @@ const navigation = document.querySelector('[data-js="navigation"]');
 const prevButton = document.querySelector('[data-js="button-prev"]');
 const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
+
+// EventListener als card Testfunktion
+
+nextButton.addEventListener("click", createCharacterCard);
 
 // States
 const maxPage = 1;
