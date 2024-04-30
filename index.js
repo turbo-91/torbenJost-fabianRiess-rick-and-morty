3165eb5b3c12839b87b36af813032759da0a3136
Torben Jost <torbenjost@proton.me>
2024-04-30 16:24:36 +0200

    final changes added

--- index.js
+++ index.js
@@ -11,9 +11,50 @@ const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
 // States
-const maxPage = 1;
-const page = 1;
-const searchQuery = "";
+let maxPage = 1;
+let page = 1;
+let searchQuery = "";
+
+async function fetchCharacters() {
+  try {
+    const response = await fetch(
+      `https:rickandmortyapi.com/api/character?page=${page}&name=${searchQuery}`
+    );
+
+    if (response.ok) {
+      const data = await response.json();
+      //console.log(data);
+      maxPage = data.info.pages;
+      pagination.textContent = `${page} / ${maxPage}`;
+      cardContainer.innerHTML = "";
+      data.results.forEach((character) => {
+        createCharacterCard(character);
+      });
+      return data;
+    } else {
+      console.error("Bad Response");
+    }
+  } catch (error) {
+    console.error("An Error occurred");
+  }
+}
+
+const data = await fetchCharacters();
+data.results.forEach((character) => {
+  createCharacterCard(character);
+});
+
+searchBar.addEventListener("submit", (event) => {
+  event.preventDefault();
+  searchQuery = searchBar.querySelector(".search-bar__input").value;
+  fetchCharacters();
+});
+
+/* searchBar.addEventListener("submit", (event) => {
+  event.preventDefault();
+  searchQuery = searchBar.querySelector(".search-bar__input").value;
+  fetchCharacters();
+}); */
 
 nextButton.addEventListener("click", () => {
   if (page < maxPage) {
@@ -27,31 +68,4 @@ prevButton.addEventListener("click", () => {
     page--;
     fetchCharacters();
   }
-});
-
-async function fetchCharacters() {
-  try {
-    const response = await fetch(
-      `https:rickandmortyapi.com/api/character?page=${page}`
-    );
-
-    if (response.ok) {
-      const data = await response.json();
-      maxPage = data.info.pages;
-      pagination.textContent = `${page} / ${maxPage}`;
-      return data;
-    } else {
-      console.error("Bad Response");
-    }
-  } catch (error) {
-    console.error("An Error occurred");
-  }
-}
-
-const data = await fetchCharacters();
-data.innerHTML = "";
-console.log(data.results);
-
-data.results.forEach((character) => {
-  createCharacterCard(character);
 });
