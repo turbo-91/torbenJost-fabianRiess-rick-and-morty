9bdff46e6ab91befeacc83c17be71f76b2caba52
Torben Jost <torbenjost@proton.me>
2024-04-30 11:03:15 +0200

    delete test event listener

--- index.js
+++ index.js
@@ -10,10 +10,6 @@ const prevButton = document.querySelector('[data-js="button-prev"]');
 const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
-// EventListener als card Testfunktion
-
-nextButton.addEventListener("click", createCharacterCard);
-
 // States
 const maxPage = 1;
 const page = 1;
